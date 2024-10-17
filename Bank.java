c3b4ec93430c6da2ddc879b709166f10d88d2dd0
evanval <60281964+evanval@users.noreply.github.com>
2024-10-16 20:20:01 -0500

    Update 1 Bank.java

--- Bank.java
+++ Bank.java
@@ -1,4 +1,28 @@
 
 public class Bank {
-
+	private int bankroll;
+	
+	public Bank(int initialBankroll) {
+		this.bankroll = initialBankroll;
+	}
+	
+	public int getBankroll() {
+		return bankroll;
+	}
+	
+	public void addWinnings(int amount) {
+		bankroll += amount;
+	}
+	
+	public boolean placeBet(int bet) {
+		if (bet <= bankroll) {
+			bankroll -= bet;
+			return true;
+		}
+		return false;
+	}
+	
+	public void printBankroll() {
+		System.out.println("Your bankroll is: " + bankroll);
+	}
 }
