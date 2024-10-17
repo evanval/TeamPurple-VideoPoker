db0da7f7fff3f7f4d7ce3ffbf28d54a542a84f75
evanval <60281964+evanval@users.noreply.github.com>
2024-10-16 20:21:14 -0500

    Update 1 Card.java

--- Card.java
+++ Card.java
@@ -1,4 +1,24 @@
 
 public class Card {
-
+	private String rank;
+	private String suit;
+	
+	
+	public Card(String rank, String suit) {
+		this.rank = rank;
+		this.suit = suit;
+	}
+	
+	public String getRank() {
+		return rank;
+	}
+	
+	public String getSuit() {
+		return suit;
+	}
+	
+	public String toString() {
+		return rank + " of " + suit;
+	}
 }
+
