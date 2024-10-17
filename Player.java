a235bc0027e32f5e431ee3244fa63d9762863db6
evanval <60281964+evanval@users.noreply.github.com>
2024-10-16 20:21:40 -0500

    Update 1 Player.java

--- Player.java
+++ Player.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Player {
-
+	private List<Card> hand;
+	
+	public Player() {
+		hand = new ArrayList<>();
+	}
+	
+	public List<Card> getHand() {
+		return hand;
+	}
+	
+	public void setHand(List<Card> hand) {
+		this.hand = hand;
+	}
+	
+	public void displayHand() {
+		System.out.println("Your Hand is: ");
+		for (Card card : hand) {
+			System.out.println(card);
+		}
+	}
 }
