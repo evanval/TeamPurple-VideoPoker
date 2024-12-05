69d499fc1c61c28aa54c26c02fffa99cc10f2cb6
evanval <60281964+evanval@users.noreply.github.com>
2024-12-05 06:38:59 -0600

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,8 +1,9 @@
+package team_purple.final_video_poker;
 
 public class Card {
 	private String rank;
 	private String suit;
-	
+	private boolean held;
 	
 	public Card(String rank, String suit) {
 		this.rank = rank;
@@ -19,5 +20,12 @@ public class Card {
 	
 	public String toString() {
 		return rank + " of " + suit;
+	}
+
+	public boolean isHeld() {
+		return held;
+	}
+	public void setHeld(boolean held) {
+		this.held = held;
 	}
 }
